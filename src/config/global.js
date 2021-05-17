8f0882ad853925bf7bece4dd7a88c0649699fe4b
scamacho84 <sergio.camachoo@misena.edu.co>
2021-05-16 19:11:35 -0500

    Avance 9
    Actualizacion de glosarios, referencias e informacion complementaria

--- src/config/global.js
+++ src/config/global.js
@@ -93,12 +93,6 @@ export default {
     ],
     subMenu: [
       {
-        nombreRuta: 'actividad',
-        icono: 'far fa-question-circle',
-        titulo: 'Actividad didáctica',
-        desarrolloContenidos: true,
-      },
-      {
         nombreRuta: 'glosario',
         icono: 'fas fa-sort-alpha-down',
         titulo: 'Glosario',
@@ -112,11 +106,6 @@ export default {
         icono: 'fas fa-book',
         titulo: 'Referencias bibliográficas',
         nombreRuta: 'referencias',
-      },
-      {
-        icono: 'fas fa-download',
-        titulo: 'Descargar material',
-        download: 'downloads/material.zip',
       },
       {
         icono: 'far fa-registered',
@@ -217,125 +206,204 @@ export default {
   },
   referencias: [
     {
+      referencia: 'Concepto.de. (s.f.). Concepto de sustancias químicas.',
+      link: 'https://concepto.de/sustancias-quimicas/',
+    },
+    {
       referencia:
-        'Zlackman, C. (2012). 100 años de moda. Sistema de Bibliotecas SENA: ',
+        'Decreto 1609 de 2002. [Ministerio de Transporte]. Por el cual se reglamenta el manejo y transporte terrestre automotor de mercancías peligrosas por carretera. Julio 31 de 2002.',
+    },
+    {
+      referencia:
+        'Decreto 1496 de 2018. [Presidente de la República de Colombia]. Por el cual se adopta el Sistema Globalmente Armonizado de clasificación y etiquetado de productos químicos y se dictan otras disposiciones en materia de seguridad química. Agosto 6 de 2018.',
+    },
+    {
+      referencia: 'Definicion.DE. (s.f). Definición de toxicidad.',
+      link: 'https://definicion.de/?s=Toxicidad',
+    },
+    {
+      referencia: 'Definicion.mx (s.f). Definición de gas.',
+      link: 'https://definicion.mx/gas/',
+    },
+    {
+      referencia:
+        'Enembalajes Nicolás. (s.f). ¿Qué es el embalaje? características, funciones, materiales [Web log post]. Blog.',
+      link: 'https://www.embalajesdemadera.net/que-es-el-embalaje/',
+    },
+    {
+      referencia:
+        'GQSP Colombia. (2020).  Guía para la elaboración de fichas de datos de seguridad (FDS). Según el Sistema Globalmente Armonizado de clasificación y etiquetado de productos químicos (SGA).',
       link:
-        'https://sena-primo.hosted.exlibrisgroup.com/primo-explore/fulldisplay?docid=sena_aleph000060086&vid=SENA&search_scope=sena_completo&tab=sena_completo&lang=es_ES&context=L',
+        'https://gqspcolombia.org/wp-content/uploads/2020/03/Onudi-Colombia_Digital_02.pdf',
     },
     {
       referencia:
-        'Castellanos, O., Fumeque A., y Ramirez D. (2011). Análisis de tendencias: de la información hacia la innovación. Universidad Nacional de Colombia. Sistema de Bibliotecas SENA: ',
+        'ICONTEC. (1998). Norma Técnica Colombiana NTC 4435. Transporte de mercancías. Hojas de seguridad para materiales. Preparación.',
       link:
-        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/1j5choe/sena_aleph000007947',
+        'https://web.mintransporte.gov.co/consultas/mercapeli/Reglamento/Anexos/NTC4435.pdf',
     },
     {
       referencia:
-        'Harpersbazaar. (2019). La ropa que eliges afecta a tu estado de ánimo y cerebro, según expertos. En arpersbazaar.mx.',
-    },
-    {
-      referencia:
-        'Lafuente M., Navarro J., y Navarro J. (2005). Ilustración de moda. Sistema de Bibliotecas SENA',
+        'Instituto Sindical de Trabajo, Ambiente y Salud. (s.f). Dañinas para el medio ambiente.',
       link:
-        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/1j5choe/sena_aleph000025549',
+        'https://istas.net/istas/riesgo-quimico/agentes-quimicos-peligrosos/lista-negra-de-sustancias-quimicas/daninas-para-el#:~:text=Entre%20los%20peligros%20para%20el,la%20atm%C3%B3sfera%20o%20el%20suelo.&text=Contaminantes%20del%20agua%3A%20incluye%20las,(sustancias%20eutrofizantes)%2C%20Ej.',
     },
     {
-      referencia:
-        'Laver, J.,  Albizua, H., y Fortea, B. (1990). Breve historia del traje y la moda. Sistema de Bibliotecas SENA. ',
-    },
-    {
-      referencia:
-        'Navarro, J.,  y Lafuente, M. (2010). Ilustración de moda: dibujo plano. Sistema de Bibliotecas SENA:',
+      referencia: 'Naciones Unidas (2015). Parte 1 Introducción.',
       link:
-        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/q6j6k0/sena_aleph000023241',
+        'http://www.unece.org/fileadmin/DAM/trans/danger/publi/ghs/ghs_rev06/Spanish/01sp_part1.pdf',
+    },
+    {
+      referencia: 'Naciones Unidas (2017). Parte 2 Peligros físicos.',
+      link:
+        'https://www.unece.org/fileadmin/DAM/trans/danger/publi/ghs/ghs_rev07/Spanish/02sp_part2.pdf',
+    },
+    {
+      referencia: 'Naciones Unidas. (2015). Parte 3 Peligros para la salud.',
+      link:
+        'http://www.unece.org/fileadmin/DAM/trans/danger/publi/ghs/ghs_rev06/Spanish/02sp_part3.pdf',
     },
     {
       referencia:
-        'Sociedad Americana de Ensayos y Materiales (ASTM). (2003). Textiles. Editorial Staff. [Book]. Sistema de Bibliotecas SENA.',
+        'Naciones Unidas. (2015). Parte 4 Peligros para el medio ambiente.',
       link:
-        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/1j5choe/sena_aleph000051551',
+        'http://www.unece.org/fileadmin/DAM/trans/danger/publi/ghs/ghs_rev06/Spanish/04sp_part4.pdf',
+    },
+    {
+      referencia: 'Thompson, I. (2009).  El empaque. Marketing-Free.com.',
+      link: 'https://www.marketing-free.com/producto/empaques.htm',
     },
     {
       referencia:
-        'Szkutnicka, B., Cuenca, C., Koyama, A. (2010). El dibujo técnico de moda paso a paso. Sistema de Bibliotecas SENA.',
+        'UNECE. (2012).  Comisión Económica de las Naciones Unidas para Europa.',
       link:
-        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/1j5choe/sena_aleph000057728',
+        'https://unece.org/DAM/trans/main/wp29/wp29wgs/wp29gen/wp29pub/WP29_Blue_Book_2012-1s_01.pdf',
+    },
+    {
+      referencia:
+        'UNECE. (2015).  Comisión Económica de las Naciones Unidas para Europa',
+      link: 'https://unece.org/es/ghs-rev6-2015',
+    },
+    {
+      referencia: 'UNECE. (2015). Parte 2 Clasificación.',
+      link:
+        'https://www.unece.org/fileadmin/DAM/trans/danger/publi/unrec/rev13/Spanish/02_Part2.pdf',
+    },
+    {
+      referencia:
+        'UNECE. (2015). Sistema Globalmente Armonizado de clasificación y etiquetado de productos químicos (SGA). Sexta edición revisada.  Naciones Unidas Nueva York y Ginebra.',
+      link:
+        'https://www.unece.org/fileadmin/DAM/trans/danger/publi/ghs/ghs_rev06/Spanish/ST-SG-AC10-30-Rev6sp.pdf',
     },
   ],
   glosario: [
     {
-      termino: 'MoodBoard',
+      termino: 'Embalaje',
       significado:
-        'su traducción literal es mapa de inspiración y aunque su uso está muy asociado al ámbito de la decoración de interiores o la moda, <br/>realmente se utiliza en entornos creativos muy diversos, desde el diseño de productos o servicios al audiovisual o el diseño gráfico.',
+        'El embalaje es todo el proceso que se lleva a cabo para proteger el producto o mercancía durante su manipulación, traslado y almacenamiento. Con este proceso se protege el producto desde el momento de su producción hasta el momento en que es consumido.',
     },
     {
-      termino: 'Suftware',
+      termino: 'Empaque',
       significado:
-        'término informático que hace referencia a un programa o conjunto de programas de cómputo, así como datos, procedimientos y pautas que permiten realizar distintas tareas en un sistema informático.',
+        'En la actualidad el "empaque" es una parte fundamental del producto porque además de contener, proteger y/o preservar el producto permitiendo que este llegue en óptimas condiciones al consumidor final, es una poderosa herramienta de promoción y venta.',
     },
     {
-      termino: 'Pirognóstico',
+      termino: 'Etiqueta',
       significado:
-        'ensayo sin componentes reactivos, quiere decir, reconocimiento de los componentes, mediante la aplicación adecuada del fuego.',
+        'Cualquier rótulo, marbete, inscripción, imagen u otra materia descriptiva o gráfica, escrita, impresa, marcada, grabada en alto o bajo relieve, adherida o sobrepuesta al producto, a su envase o, cuando no sea posible el embalaje por las características del producto o su envase.',
     },
     {
-      termino: 'Vectores',
+      termino: 'Ficha de datos de seguridad (FDS)',
       significado:
-        'en software de diseño, son líneas que se crean y permiten, expandir, modificar, al tamaño que sea sin pixelarse porque se unen por coordenadas matemáticas del programa y no se generan por números fijos.',
+        'Es un documento que proporciona información completa sobre una sustancia o mezcla con miras al control y reglamentación de su utilización en el lugar de trabajo.',
     },
     {
-      termino: 'Moda',
+      termino: 'Gases',
       significado:
-        'hábito repetitivo que identifica a una persona, una especie de presión social, que debe consumir, utilizar o hacer.',
+        'Un gas es una sustancia que está en un estado en donde sus moléculas tienen una relación muy débil entre sí. En este sentido se diferencia de un líquido o un sólido, remitiendo estos dos casos a sustancias en las que las moléculas tienen una relación más estrecha.',
     },
     {
-      termino: 'Diseño',
+      termino: 'Sustancias químicas',
       significado:
-        'actividad creativa orientada a soluciones idóneas a ciertas problemáticas.',
+        'Se entiende por sustancia química o especie química un tipo de materia que es químicamente homogénea y definida, es decir, que posee una composición química fija, dependiendo del contexto, pueden ser sinónimos de las sustancias simples (opuestas a sustancias compuestas), formadas por átomos de un único tipo.',
     },
     {
-      termino: 'Tipología',
+      termino: 'Toxicidad',
       significado:
-        'estudio de los tipos o modelos que se usan para clasificar en diversas ciencias o disciplinas.',
-    },
-    {
-      termino: 'Bidimensional',
-      significado:
-        'que se representa según su altura y su anchura y no su profundidad.',
-    },
-    {
-      termino: 'Software',
-      significado:
-        'término informático que hace referencia a un programa o conjunto de programas de cómputo, así como datos, procedimientos y pautas que permiten realizar distintas tareas en un sistema informático.',
+        'Se denomina toxicidad al grado de efectividad que poseen las sustancias que, por su composición, se consideran tóxicas. Se trata de una medida que se emplea para identificar el nivel tóxico de diversos fluidos o elementos, afectando tanto a un organismo en su totalidad, por ejemplo, el cuerpo del ser humano, como sobre una subestructura (una célula)',
     },
   ],
   complementario: [
     {
       texto:
-        'BIC., (2015). “2 minutos para entender el desarrollo sostenible – Spanish”',
-      tipo: 'Video',
-      link: 'https://www.google.com/',
+        'GQSP Colombia. (2020).  Guía para la elaboración de fichas de datos de seguridad (FDS). Según el Sistema Globalmente Armonizado de clasificación y etiquetado de productos',
+      tipo: 'PDF',
+      link:
+        'https://gqspcolombia.org/wp-content/uploads/2020/03/Onudi-Colombia_Digital_02.pdf',
     },
     {
       texto:
-        'Arnold, M. y Osorio, F., (1998). Introducción a los conceptos básicos de la Teoría general de sistemas. Cinta moebio 3: 40-49',
-      tipo: 'Página web',
-      descarga: '/downloads/prueba.pdf',
+        'ICONTEC. (1998). Norma Técnica Colombiana NTC 4435. Transporte de mercancías. Hojas de seguridad para materiales. Preparación.',
+      tipo: 'PDF',
+      link:
+        'https://web.mintransporte.gov.co/consultas/mercapeli/Reglamento/Anexos/NTC4435.pdf',
     },
     {
       texto:
-        'Balboa, C. y Dominguez, M. (2014). Economía circular como marco para el ecodiseño: el modelo ECO-3. Informador Técnico (Colombia) Volumen 78, No. 1. Pág. 52-90.',
-      tipo: 'Artículo científico',
-      link: 'https://www.google.com/',
-    },
-    {
-      texto: 'Economía de Rosquilla. (s.f.).',
+        'Instituto Sindical de Trabajo, Ambiente y Salud. (s.f). Dañinas para el medio ambiente.',
       tipo: 'Página web',
-      descarga: '/downloads/prueba.pdf',
+      link:
+        'https://istas.net/istas/riesgo-quimico/agentes-quimicos-peligrosos/lista-negra-de-sustancias-quimicas/daninas-para-el#:~:text=Entre%20los%20peligros%20para%20el,la%20atm%C3%B3sfera%20o%20el%20suelo.&text=Contaminantes%20del%20agua%3A%20incluye%20las,(sustancias%20eutrofizantes)%2C%20Ej.',
     },
     {
-      texto: '2 minutos para entender el desarrollo sostenible – Spanish',
-      tipo: 'Video',
-      link: 'https://www.google.com/',
+      texto: 'Naciones Unidas (2015). Parte 1 Introducción.',
+      tipo: 'PDF',
+      link:
+        'http://www.unece.org/fileadmin/DAM/trans/danger/publi/ghs/ghs_rev06/Spanish/01sp_part1.pdf',
+    },
+    {
+      texto: 'Naciones Unidas (2017). Parte 2 Peligros físicos.',
+      tipo: 'PDF',
+      link:
+        'https://www.unece.org/fileadmin/DAM/trans/danger/publi/ghs/ghs_rev07/Spanish/02sp_part2.pdf',
+    },
+    {
+      texto: 'Naciones Unidas (2017). Parte 3 Peligros para la salud.',
+      tipo: 'PDF',
+      link:
+        'http://www.unece.org/fileadmin/DAM/trans/danger/publi/ghs/ghs_rev06/Spanish/02sp_part3.pdf',
+    },
+    {
+      texto: 'Naciones Unidas (2017). Parte 4 Peligros para el medio ambiente.',
+      tipo: 'PDF',
+      link:
+        'http://www.unece.org/fileadmin/DAM/trans/danger/publi/ghs/ghs_rev06/Spanish/04sp_part4.pdf',
+    },
+    {
+      texto:
+        'UNECE. (2012).  Comisión Económica de las Naciones Unidas para Europa.',
+      tipo: 'PDF',
+      link:
+        'https://unece.org/DAM/trans/main/wp29/wp29wgs/wp29gen/wp29pub/WP29_Blue_Book_2012-1s_01.pdf',
+    },
+    {
+      texto:
+        'UNECE. (2015).  Comisión Económica de las Naciones Unidas para Europa.',
+      tipo: 'Página web',
+      link: 'https://unece.org/es/ghs-rev6-2015',
+    },
+    {
+      texto: 'UNECE. (2015). Parte 2 Clasificación.',
+      tipo: 'PDF',
+      link:
+        'https://www.unece.org/fileadmin/DAM/trans/danger/publi/unrec/rev13/Spanish/02_Part2.pdf',
+    },
+    {
+      texto:
+        'UNECE. (2015). Sistema Globalmente Armonizado de clasificación y etiquetado de productos químicos (SGA). Sexta edición revisada.  Naciones Unidas Nueva York y Ginebra.',
+      tipo: 'PDF',
+      link:
+        'https://www.unece.org/fileadmin/DAM/trans/danger/publi/ghs/ghs_rev06/Spanish/ST-SG-AC10-30-Rev6sp.pdf',
     },
   ],
 }
